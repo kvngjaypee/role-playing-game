e0b79d4d03c96e335692495763c8be3b2d75a4bc
kvngjaypee <johnpaulanyanwu17@gmail.com>
2023-12-30 00:48:50 +0100

    work in progress

--- main.js
+++ main.js
@@ -91,6 +91,11 @@ function buyHealth() {
 
 function buyWeapon() {
   if (gold >= 30) {
+    gold -= 30;
+    currentWeapon++;
+    goldText.innerText = gold;
+    let newWeapon = weapons[currentWeapon].name;
+    text.innerText = "You now have a new weapon.";
   }
 }
 
